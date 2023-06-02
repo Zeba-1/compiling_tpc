5f8ce3b2d8ca125ef4d7ac5585f9f30cb386a6bd
Sebastien Sautier <sebsautier18@gmail.com>
2023-06-02 16:19:45 +0200

    pour le rendu

--- src/gram.tab.h
+++ src/gram.tab.h
@@ -1,14 +1,14 @@
-/* A Bison parser, made by GNU Bison 3.7.5.  */
+/* A Bison parser, made by GNU Bison 2.3.  */
 
-/* Bison interface for Yacc-like parsers in C
+/* Skeleton interface for Bison's Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
-   Inc.
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
 
-   This program is free software: you can redistribute it and/or modify
+   This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,9 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -31,69 +33,64 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
-   especially those whose name start with YY_ or yy_.  They are
-   private implementation details that can be changed or removed.  */
-
-#ifndef YY_YY_SRC_GRAM_TAB_H_INCLUDED
-# define YY_YY_SRC_GRAM_TAB_H_INCLUDED
-/* Debug traces.  */
-#ifndef YYDEBUG
-# define YYDEBUG 0
-#endif
-#if YYDEBUG
-extern int yydebug;
-#endif
-
-/* Token kinds.  */
+/* Tokens.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-  enum yytokentype
-  {
-    YYEMPTY = -2,
-    YYEOF = 0,                     /* "end of file"  */
-    YYerror = 256,                 /* error  */
-    YYUNDEF = 257,                 /* "invalid token"  */
-    VOID = 258,                    /* VOID  */
-    IF = 259,                      /* IF  */
-    ELSE = 260,                    /* ELSE  */
-    WHILE = 261,                   /* WHILE  */
-    RETURN = 262,                  /* RETURN  */
-    IDENT = 263,                   /* IDENT  */
-    TYPE = 264,                    /* TYPE  */
-    AND = 265,                     /* AND  */
-    EQ = 266,                      /* EQ  */
-    OR = 267,                      /* OR  */
-    ORDER = 268,                   /* ORDER  */
-    CHARACTER = 269,               /* CHARACTER  */
-    NUM = 270,                     /* NUM  */
-    ADDSUB = 271,                  /* ADDSUB  */
-    DIVSTAR = 272                  /* DIVSTAR  */
-  };
-  typedef enum yytokentype yytoken_kind_t;
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     VOID = 258,
+     IF = 259,
+     ELSE = 260,
+     WHILE = 261,
+     RETURN = 262,
+     IDENT = 263,
+     TYPE = 264,
+     AND = 265,
+     EQ = 266,
+     OR = 267,
+     ORDER = 268,
+     CHARACTER = 269,
+     NUM = 270,
+     ADDSUB = 271,
+     DIVSTAR = 272
+   };
 #endif
+/* Tokens.  */
+#define VOID 258
+#define IF 259
+#define ELSE 260
+#define WHILE 261
+#define RETURN 262
+#define IDENT 263
+#define TYPE 264
+#define AND 265
+#define EQ 266
+#define OR 267
+#define ORDER 268
+#define CHARACTER 269
+#define NUM 270
+#define ADDSUB 271
+#define DIVSTAR 272
 
-/* Value type.  */
+
+
+
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-union YYSTYPE
-{
+typedef union YYSTYPE
 #line 16 "src/gram.y"
-
+{
   Node* node;
   char* name;
   int const_val;
-
-#line 87 "src/gram.tab.h"
-
-};
-typedef union YYSTYPE YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
+}
+/* Line 1529 of yacc.c.  */
+#line 89 "src/gram.tab.h"
+	YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
 #endif
-
 
 extern YYSTYPE yylval;
 
-int yyparse (void);
-
-#endif /* !YY_YY_SRC_GRAM_TAB_H_INCLUDED  */
--- src/main.c
+++ src/main.c
@@ -26,10 +26,43 @@ void print_help() {
     printf("./tpcas [-OPTION] < [FILE]\n");
 }
 
+void printSymbTab(Prog_symb_tab prog_tab) {
+    int i, j;
+    
+    printf("GLOBAL SYMB TAB:\n");
+    /* global tab */
+    for (i = 0; i < prog_tab.global_tab.size_tab; i++) {
+        printf("- (%s) %s : ", prog_tab.global_tab.var_tab[i].type, prog_tab.global_tab.var_tab[i].name);
+        if (prog_tab.global_tab.var_tab[i].fun)
+            printf("function\n");
+        else if (prog_tab.global_tab.var_tab[i].param)
+            printf("parameters\n");
+        else
+            printf("global variable\n");
+    }
+    printf("\n");
+
+    /* each function tab */
+    for (i = 0; i < prog_tab.size_tab; i++) {
+        printf("%s SYMB TAB:\n", prog_tab.symb_tab[i].fun_name);
+        for (j = 0; j < prog_tab.symb_tab[i].size_tab; j++) {
+            printf("- (%s) %s : ", prog_tab.symb_tab[i].var_tab[j].type, prog_tab.symb_tab[i].var_tab[j].name);
+            if (prog_tab.symb_tab[i].var_tab[j].fun)
+                printf("function\n");
+            else if (prog_tab.symb_tab[i].var_tab[j].param)
+                printf("parameters\n");
+            else
+                printf("variable\n");
+        }
+        printf("\n");
+    }
+}
+
 int main(int argc, char const *argv[]) {
     int i;
     int help = 0;
     int tree = 0;
+    int symbtabs = 0;
     int stdinRedifined = 0;
 
     Prog_symb_tab prog_symb_tab;
@@ -41,6 +74,8 @@ int main(int argc, char const *argv[]) {
             help = 1;
         else if(strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--tree") == 0)
             tree = 1;
+        else if(strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--symtabs") == 0)
+            symbtabs = 1;
         else {
             // We redifine stdin with the first name we meet
             if (!stdinRedifined) {
@@ -79,8 +114,12 @@ int main(int argc, char const *argv[]) {
     if (check_semantique(Tree->firstChild, &prog_symb_tab))
         return 2;
 
+    if (symbtabs) {
+        printSymbTab(prog_symb_tab);
+    }
+
     /* traduction */
-    trad_nasm(Tree, prog_symb_tab, "nasm/trad.asm");
+    trad_nasm(Tree, prog_symb_tab, "FILE.asm");
 
     free(Tree);
     return 0;
